b85e4e90ea5dc72df9e7436560c0f10a2ef30136
Bkmetoff <bkmetoff@gmail.com>
2023-07-03 18:16:44 +0300

    Fixes CloudWatch alarm

--- modules/VPC/cloudwatch.tf
+++ modules/VPC/cloudwatch.tf
@@ -1,20 +1,30 @@
-# https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/rds-metrics.html
+# https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-cloudwatch-metrics.html#target-metric-table
 
-resource "aws_cloudwatch_metric_alarm" "rds_requests_alarm" {
-  alarm_description   = "Monitors the number of successful or failing attempts to connect to an instance."
-  alarm_name          = "${var.name}-cloudwatch-rds-requests-alarm"
+resource "aws_cloudwatch_metric_alarm" "requests_alarm" {
+  count = length(var.availability_zones)
+
+  # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html
+  # The configuration below is for demonstration purposes.
+  #
+  # Evaluate a period of 10 minutes.
+  # if more than 5 requests hit the ELB target group within
+  # a single 10-minute period, the alarm will go to ALARM state.
+  datapoints_to_alarm = "1"
+  evaluation_periods  = "1"
+  threshold           = "20"
+  period              = "600"
+
+  alarm_description   = "Monitors the number of requests received by each target in an ELB target group."
+  alarm_name          = "${var.name}-cloudwatch-elb-target-requests-alarm-${count.index}"
   comparison_operator = "GreaterThanThreshold"
-  datapoints_to_alarm = "2"
-  evaluation_periods  = "3"
-  namespace           = "AWS/RDS"
-  metric_name         = "ConnectionAttempts"
-  period              = "10"
+  namespace           = "AWS/ApplicationELB"
+  metric_name         = "RequestCountPerTarget"
   statistic           = "Sum"
-  threshold           = "5"
   treat_missing_data  = "missing"
-  unit                = "Count"
   dimensions = {
-    DBInstanceIdentifier = var.rds_instance_id
+    TargetGroup      = aws_lb_target_group.lb_tg.arn_suffix
+    LoadBalancer     = aws_lb.lb.arn_suffix
+    AvailabilityZone = var.availability_zones[count.index]
   }
 }
 

6acf4615fc44635ece532457ec4d328e01875ff2
Bkmetoff <bkmetoff@gmail.com>
2023-07-03 20:03:06 +0300

    Removes code comments from bin scripts

--- bin/build_dk_image.sh
+++ bin/build_dk_image.sh
@@ -8,6 +8,4 @@
 # $2 == aws region
 # $3 == ecr repo name
 
-
-# ./bin/build_dk_image.sh  936892409162 eu-west-1 swo-assignment
 docker build . -t "$1.dkr.ecr.$2.amazonaws.com/$3" -f $(pwd)/Dockerfile
--- bin/push_dk_image_to_ecr.sh
+++ bin/push_dk_image_to_ecr.sh
@@ -11,7 +11,6 @@
 # https://docs.aws.amazon.com/cli/latest/reference/ecr/get-login-password.html
 # not suitable for production environments!!
 
-# ./bin/push_dk_image_to_ecr.sh  936892409162 eu-west-1 swo-assignment
 echo "=== Authenticating to ECR ==="
 echo $(aws ecr get-login-password --region $2 | docker login --username AWS --password-stdin $1.dkr.ecr.$2.amazonaws.com)
 
